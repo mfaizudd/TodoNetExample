065f1c3a93079c029d7dd27d7d6c507eb34c8980
mfaizudd <mfaizudd@gmail.com>
2021-11-07 19:53:07 +0700

    fix: use statsu code pages

--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,8 @@ namespace TodoNetExample
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePages();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
