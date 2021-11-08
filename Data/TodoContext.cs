af04b84f18d771c5af8f2001cc220c1bba57262e
mfaizudd <mfaizudd@gmail.com>
2021-11-08 09:45:58 +0700

    fix: prevent generated values from seeder

--- Data/TodoContext.cs
+++ Data/TodoContext.cs
@@ -17,19 +17,22 @@ namespace TodoNetExample.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
             builder.Entity<IdentityRole>()
                 .HasData(new [] {
                     new IdentityRole 
                     {
                         Id = "2ec3739c-0f45-4357-b178-7f797858d10f",
                         Name = "Admin",
-                        NormalizedName = "ADMIN"
+                        NormalizedName = "ADMIN",
+                        ConcurrencyStamp = "b67e4a95-7d86-4f21-a8a1-765027850a80"
                     },
                     new IdentityRole
                     {
                         Id = "153109d6-ef68-46c3-a8ef-491a85c55bfa",
                         Name = "User",
-                        NormalizedName = "USER"
+                        NormalizedName = "USER",
+                        ConcurrencyStamp = "e4499df5-13db-4f01-a4b7-82ec2e32950f"
                     }
                 });
 
@@ -42,9 +45,12 @@ namespace TodoNetExample.Data
                 NormalizedUserName = "ADMIN@ADMIN.COM",
                 Email = "admin@admin.com",
                 NormalizedEmail = "ADMIN@ADMIN.COM",
-                EmailConfirmed = true
+                EmailConfirmed = true,
+                PasswordHash = "AQAAAAEAACcQAAAAEFc1qYDPzPcQyUCJAlgNm33VY1aOFCx9rKlWGt4kyGkJGhdIn+sJJFXHiZR4Vi+ZoQ==",
+                SecurityStamp = "81a0eaf1-b7a1-41d0-8bb7-af103d441e82",
+                ConcurrencyStamp = "bd0951e9-e6c7-4f08-b9e1-11dc519682f8"
             };
-            admin.PasswordHash = hasher.HashPassword(admin, "Test123!");
+
             builder.Entity<User>()
                 .HasData(admin);
             
